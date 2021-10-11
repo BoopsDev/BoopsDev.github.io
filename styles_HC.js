9edfb9bdb5f1db7e34a1849f749151413bb8f6c7
MID-lab <m.dylan@outlook.fr>
2021-10-11 14:40:00 +0200

    Update styles_HC.js

--- styles_HC.js
+++ styles_HC.js
@@ -36,10 +36,11 @@ sectionNum.addEventListener('load', (event) => {
 });*/
 
 window.addEventListener('load', (event) => {
+    console.log('Logo !');
+
     let logo = document.querySelector('#home-products');
 
     logo.addEventListener('load', (event) => {
         console.log('Logo has been loaded!');
     });
-    console.log('Logo !');
 });
