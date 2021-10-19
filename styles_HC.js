5a50fee337363413519ce634591a26cdc4c0c556
MID-lab <m.dylan@outlook.fr>
2021-10-19 14:02:10 +0200

    Update styles_HC.js

--- styles_HC.js
+++ styles_HC.js
@@ -39,7 +39,7 @@ $("#home-products").on("change", function () {
 });
 
 $(document).ready(function () {
-    var x = $("div#products").position();
+    var x = $("div#products").offset();
     console.log(x);
 });
 /*$(document).ready(function(){
