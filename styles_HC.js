2802432bf9fb587527ea5b7f05aa11cadb7c6928
MID-lab <m.dylan@outlook.fr>
2021-10-20 11:49:23 +0200

    Update styles_HC.js

--- styles_HC.js
+++ styles_HC.js
@@ -16,62 +16,6 @@ if (!document.getElementById(cssId)) {
     head.appendChild(link);
 };
 
-/*window.onload = function () {
-    $('#home-products .home-products-item:nth-child(odd)').addClass('.section1-2');
-};*/
-
-//document.addEventListener('loaded', init);
-
-/*window.addEventListener('load', (event) => {
-    let logo = document.querySelector('#home-products');
-
-    logo.addEventListener('load', (event) => {
-        console.log('Logo has been loaded!');
-        alert('evzkmevze');
-    });
-});*/
-
-
-//var divPoss = document.getElementById("products");
-
-// Select the node that will be observed for mutations
-const targetNode = $('#products')[0];
-
-// Options for the observer (which mutations to observe)
-const config = { attributes: true, childList: true, subtree: true };
-
-// Callback function to execute when mutations are observed
-const callback = function (mutationsList, observer) {
-    // Use traditional 'for loops' for IE 11
-    for (const mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            console.log('A child node has been added or removed.');
-        }
-        else if (mutation.type === 'attributes') {
-            console.log('The ' + mutation.attributeName + ' attribute was modified.');
-        }
-    }
-};
-
-// Create an observer instance linked to the callback function
-const observer = new MutationObserver(callback);
-
-// Start observing the target node for configured mutations
-observer.observe(targetNode, config);
-
-// Later, you can stop observing
-observer.disconnect();
-
-
-/*$(document).on('DOMNodeInserted', function (e) {
-    var test = $('.home-products-item');
-    console.log(test.length + " daz");
-    if (e.target.id == "products") {
-        //console.log(divPoss.scrollLeft);
-        console.log(test.length);
-    }
-});*/
-
 /*$(document).ready(function(){
     $.ajax({
         //L'URL de la requête
