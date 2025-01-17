56672adf040d7bbc478c9e25eac6578187e78345
MID-lab <m.dylan@outlook.fr>
2025-01-17 10:23:33 +0100

    Update styles_HC.js

--- styles_HC.js
+++ styles_HC.js
@@ -1,11 +1,5 @@
-var cssId = 'myCss';  // you could encode the css path itself to generate id..
-if (!document.getElementById(cssId)) {
-    var head = document.getElementsByTagName('head')[0];
-    var link = document.createElement('link');
-    link.id = cssId;
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    link.href = 'http://127.0.0.1:5500/BoopsDev.github.io/NewCSS.css';
-    link.media = 'all';
-    head.appendChild(link);
+function initialisation() {
+    console.log("Style CSS issue de Health&Co");
 };
+
+initialisation ();
